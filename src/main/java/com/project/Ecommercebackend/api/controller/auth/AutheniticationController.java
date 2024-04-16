80dd6a58fc51ab3be70d2a20e8ddae4f1d798fc7
Diana-Kieru <61355955+Diana-Kieru@users.noreply.github.com>
2024-04-16 22:02:42 +0300

    added controller register

--- src/main/java/com/project/Ecommercebackend/api/controller/auth/AutheniticationController.java
+++ src/main/java/com/project/Ecommercebackend/api/controller/auth/AutheniticationController.java
@@ -1,6 +1,7 @@
 package com.project.Ecommercebackend.api.controller.auth;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 
 public class AutheniticationController {
+
+  @PostMapping("/register")
+  public void registeruser(){
+
+  }
 
 }
